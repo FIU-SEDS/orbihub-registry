0c144ce169132052666768c164dc52929609b513
Eriel Cabrera <eriel.dev@pm.me>
2026-03-22 22:46:10 -0400

    created server and added initial routing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,38 @@
-package server
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/erielC/orbihub-registry/internal/model"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	apps := model.GetApps()
+	// fmt.Println(apps)
+
+	jsonBytes, err := json.Marshal(apps)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(jsonBytes))
+	
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(apps)
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Status: Ok!")
+	})
+
+	fmt.Println("Starting server at http://localhost:8000")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+
+	fmt.Println()
+}
--- internal/model/app.go
+++ internal/model/app.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"fmt"
+	"net/http"
+)
+
 type App struct {
 	ID          string `json:"id"`
 	Name        string `json:"name"`
@@ -9,4 +14,34 @@ type App struct {
 	Author      string `json:"author"`
 	Image       string `json:"image"`
 	CreatedAt   string `json:"created_at"`
+}
+
+var Apps = []App{
+	{
+		ID:          "telemetry-viewer",
+		Name:        "Orbiview",
+		Description: "Real-time rocket telemetry monitoring",
+		Version:     "1.0.0",
+		Repo:        "https://github.com/FIU-SEDS/Orbiview",
+		Author:      "Tomas Mejia",
+		Image:       "dashboard_logo.png",
+		CreatedAt:   "2026",
+	},
+}
+
+func GetApps() []App {
+	return Apps
+}
+
+func PrintAppInfo(apps []App, w http.ResponseWriter) {
+	for _, a := range apps {
+		fmt.Fprintf(w, "ID: %s\n", a.ID)
+		fmt.Fprintf(w, "Name: %s\n", a.Name)
+		fmt.Fprintf(w, "Description: %s\n", a.Description)
+		fmt.Fprintf(w, "Version: %s\n", a.Version)
+		fmt.Fprintf(w, "Repo: %s\n", a.Repo)
+		fmt.Fprintf(w, "Author: %s\n", a.Author)
+		fmt.Fprintf(w, "Image: %s\n", a.Image)
+		fmt.Fprintf(w, "Created At: %s\n\n", a.CreatedAt)
+	}
 }
